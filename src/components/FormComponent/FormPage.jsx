d48aff2c6da5c4f417209c2dc03c2501a340cab5
Elio Rodriguez <rodriguezelio2@gmail.com>
2023-11-16 17:35:37 -0300

    mod de App.css / useState para datos de formulario-imprime en consola

--- src/components/FormComponent/FormPage.jsx
+++ src/components/FormComponent/FormPage.jsx
@@ -1,41 +1,69 @@
+import { useState } from "react";
+
+
 export default function FormPage() {
+
+  const [datosForm, setDatosForm] = useState({
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(datosForm); 
+  };
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setDatosForm({
+      ...datosForm,
+      [name]: value,
+    });
+  };
+
+
+
   return (
     <>
-       <div className="body-form">
-      <div className="container-form">
-        <h1 className="title-form text-light fw-bold">Registrate!</h1>
-        <form action="">
-          <div className="form-group">
-            <label htmlFor="nombre">Nombre:</label>
-            <input type="text" className="form-control" name="nombre" required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="apellido">Apellido:</label>
-            <input type="text" className="form-control" name="apellido" required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" className="form-control" name="email" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="telefono">Teléfono:</label>
-            <input type="tel" className="form-control" name="telefono" required  />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password :</label>
-            <input type="password" className="form-control" name="password" required  />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Confirmar Password :</label>
-            <input type="password" className="form-control" name="password" required  />
-          </div>
-          <div className="btn-container">
-            <button type="reset" className="btn btn-form"> Reiniciar</button>
-            <button type="submit" className="btn btn-form">Enviar</button>
-          </div>
-        </form>
+      <div className="body-form">
+        <div className="container-form">
+          <h1 className="title-form text-light fw-bold">Registrate!</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="nombre">Nombre:</label>
+              <input type="text" className="form-control" onChange={handleInput} name="nombre" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="apellido">Apellido:</label>
+              <input type="text" className="form-control" onChange={handleInput} name="apellido" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" className="form-control" onChange={handleInput} name="email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="telefono">Teléfono:</label>
+              <input type="tel" className="form-control" onChange={handleInput} name="telefono" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password :</label>
+              <input type="password" className="form-control" onChange={handleInput} name="password" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Confirmar Password :</label>
+              <input type="password" className="form-control" onChange={handleInput} name="password" required />
+            </div>
+            <div className="btn-container">
+              <button type="reset" className="btn btn-form"> Reiniciar</button>
+              <button type="submit" className="btn btn-form">Enviar</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   );
 }
