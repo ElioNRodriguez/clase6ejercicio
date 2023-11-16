b96ab3e704a3018cbfc41411ae682811005c1e54
miguedorta7 <75344292+miguedorta7@users.noreply.github.com>
2023-11-16 17:29:17 -0300

    Update Navbar.jsx

--- src/components/HeaderComponent/Navbar.jsx
+++ src/components/HeaderComponent/Navbar.jsx
@@ -4,6 +4,36 @@ import { Link as Anchor } from "react-router-dom";
 export const Navbar = () => {
   return (
     <>
+      <div className="container-navbar">
+      <nav className="navbar navbar-expand-sm bg-gray">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#"></a>
+          <button
+            className="navbar-toggler "
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse " id="navbarNav">
+            <ul className="navbar-nav ">
+              <li className="nav-item">
+                <Anchor to={"/"} className="nav-link active text-light" aria-current="page" href="/">Home</Anchor>
+              </li>
+              <li className="nav-item">
+                <Anchor to={"/card"} className="nav-link text-light" href="/card">
+                  Card MyApp
+                </Anchor>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
     </>
   );
 };
